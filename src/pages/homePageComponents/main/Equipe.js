23f09e5f68fa35e7ece17c8044a50fcf9c3c37af
ABDELILAH <77416814+BrightProgrammer7@users.noreply.github.com>
2023-10-14 21:21:54 +0100

    Update Equipe.js

--- src/pages/homePageComponents/main/Equipe.js
+++ src/pages/homePageComponents/main/Equipe.js
@@ -4,9 +4,35 @@ export default function Equipe() {
     return (
         <div><section id="doctors" class="doctors section-bg">
             <div class="container" data-aos="fade-up">
-
+                
                 <div class="section-title">
-                    <h2>L'Équipe</h2>
+                    <h2>Le Nouveau Équipe</h2>
+                    <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+                </div>
+
+                <div class="row">
+
+                    <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+                        <div class="member" data-aos="fade-up" data-aos-delay="100">
+                            <div class="member-img">
+                                <img src="assets/img/equipe/abdelilah.jpg" class="img-fluid" alt="" />
+                                <div class="social">
+                                    <a href=""><i class="bi bi-twitter"></i></a>
+                                    <a href=""><i class="bi bi-facebook"></i></a>
+                                    <a href=""><i class="bi bi-instagram"></i></a>
+                                    <a href=""><i class="bi bi-linkedin"></i></a>
+                                </div>
+                            </div>
+                            <div class="member-info">
+                                <h4>AKHMIM ABDELILAH</h4>
+                                <span>Chief PROJET
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="section-title">
+                    <h2>L'Ancien Équipe</h2>
                     <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
                 </div>
 
